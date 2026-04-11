da891cfa3ca09f94b979ec1a3b87c1ac7f4998c5
Lucas Gomes Dias <lucasgomesdias1404@gmail.com>
2026-04-11 18:55:52 -0300

    feat: Definição de local por ID em SetPlaceMeasureReqDto

--- src/dtos/place/SetPlaceMeasureReq.dto.ts
+++ src/dtos/place/SetPlaceMeasureReq.dto.ts
@@ -1,15 +1,52 @@
 export class SetPlaceMeasureReqDto {
-    private name : string;
+    private id? : number;
+    private name? : string;
 
-	constructor($name: string) {
-		this.name = $name;
+    /**
+     * 
+     * @param {string} $name 
+     */
+	constructor($name: string)
+
+    /**
+     * 
+     * @param {number} $id 
+     */
+    constructor($id: number)
+
+    /**
+     * 
+     * @param {string | number} $value 
+     */
+    constructor($value: string | number) {
+        if (typeof $value == 'string')
+            this.name = $value;
+        else if (typeof $value == 'number') {
+            this.$id = $value;
+        }
+    }
+
+    /**
+     * Getter $id
+     * @return {number}
+     */
+	public get $id(): number | undefined {
+		return this.id;
 	}
 
+    /**
+     * Setter $id
+     * @param {number} value
+     */
+	public set $id(value: number) {
+		this.id = value;
+	}
+    
     /**
      * Getter $name
      * @return {string}
      */
-	public get $name(): string {
+	public get $name(): string | undefined {
 		return this.name;
 	}
 

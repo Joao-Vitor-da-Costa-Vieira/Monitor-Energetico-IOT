ff17e07c9a12ab342c444c9b50daf4da9e8165b9
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-04-25 19:15:32 -0300

    feat: components de Input

---
+++ src/frontEnd/MonitorIOT/app/Dispositivo.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Dispositivo = () => {
+  return (
+    <View>
+      <Text>Dispositivo</Text>
+    </View>
+  )
+}
+
+export default Dispositivo
+
+const styles = StyleSheet.create({})
---
+++ src/frontEnd/MonitorIOT/components/Inputs.jsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
+
+const emailInput = () =>{
+    return(
+        <View style={styles.padding}>
+            <TextInput style={styles.input} placeholder="Email" />
+        </View>
+    )
+}
+
+const passwordInput = () =>{
+    return(
+        <View style={styles.padding}>
+            <TextInput style={styles.input} placeholder="Senha" secureTextEntry />
+        </View>
+    )
+}
+
+export {emailInput, passwordInput};
+
+const styles = StyleSheet.create({
+    padding: {
+        padding: 10
+    },
+    input: {
+        borderWidth: 5,
+        width: 250,
+        height: 47,
+        borderColor: 'black',
+        padding: 10,
+        borderRadius: 10
+    }
+})

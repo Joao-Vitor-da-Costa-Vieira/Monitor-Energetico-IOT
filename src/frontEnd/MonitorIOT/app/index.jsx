f95548a7f7dd164d490845af44232d5e938b0c8e
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-04-25 19:34:55 -0300

    feat: tela de Login

--- src/frontEnd/MonitorIOT/app/index.jsx
+++ src/frontEnd/MonitorIOT/app/index.jsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, Scr } from 'react-native'
 import React from 'react'
+import {emailInput, passwordInput} from '../components/Inputs'
 
-const Home = () => {
+const Login = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <View style={styles.container}>
+      <Image 
+      style={{width: 100, height: 150}} source={require('../assets/adaptive-icon.png')} />
+      
+      <Text style={styles.title}>Login</Text>
+
+      {emailInput()}
+      {passwordInput()}
+
+      <View>
+        <Text>Esqueci minha senha</Text>
+      </View>
+    </View>   
   )
 }
 
-export default Home
+export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20
+  }
+})

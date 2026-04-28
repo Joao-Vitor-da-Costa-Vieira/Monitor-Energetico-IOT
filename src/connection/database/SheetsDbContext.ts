459be25e9cc2d15aa7d312d330d64855e507f19b
Lucas Gomes Dias <lucasgomesdias1404@gmail.com>
2026-04-28 11:16:07 -0300

    feat: Melhoria de tratamento de erros em SheetsDbContext

--- src/connection/database/SheetsDbContext.ts
+++ src/connection/database/SheetsDbContext.ts
@@ -1,69 +1,91 @@
 import { google, sheets_v4 } from "googleapis";
 import path from "node:path";
+import { GoogleSheetsError } from "../../errors/sheets/GoogleSheets.error.ts"
 
 class SheetsDbContext {
     private static api: sheets_v4.Sheets;
     private static initializing: Promise<void> | null;
 
     private static async getDbContext() : Promise<sheets_v4.Resource$Spreadsheets> {
-        if (!this.api) {
-            if (!this.initializing) {
-                this.initializing = this.init();
+        try {
+            if (!this.api) {
+                if (!this.initializing) {
+                    this.initializing = this.init();
+                }
+                await this.initializing;
             }
-            await this.initializing;
+            return this.api.spreadsheets;
+        } catch (e) {
+            throw e;
         }
-        return this.api.spreadsheets;
     }
 
     private static async init() {
-        const auth = new google.auth.GoogleAuth({
-            keyFile: path.resolve(process.cwd(), "environment", "credenciais.json"),
-            scopes: ["https://www.googleapis.com/auth/spreadsheets"]
-        });
+        try {
+            const auth = new google.auth.GoogleAuth({
+                keyFile: path.resolve(process.cwd(), "environment", "credenciais.json"),
+                scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+            });
 
-        this.api = google.sheets({
-            version: "v4",
-            auth
-        });
+            this.api = google.sheets({
+                version: "v4",
+                auth
+            });
+        } catch (e: any) {
+            throw new GoogleSheetsError(500, e.message);
+        }
     }
 
     public static async get($range: string) {
-        const sheet = await this.getDbContext();
-        
-        return sheet.values.get({
-            spreadsheetId: process.env.SHEETS_URL,
-            range: $range
-        })
+        try {
+            const sheet = await this.getDbContext();
+            
+            return sheet.values.get({
+                spreadsheetId: process.env.SHEETS_URL,
+                range: $range
+            })
+        } catch (e: any) {
+            throw new GoogleSheetsError(500, e.message);
+        }
     }
 
     public static async update($values: any[][], $range: string) {
-        const sheet = await this.getDbContext();
+        try {
+            const sheet = await this.getDbContext();
 
-        return sheet.values.update({
-            spreadsheetId: process.env.SHEETS_URL,
-            range: $range,
-            valueInputOption: 'USER_ENTERED',
-            includeValuesInResponse: true,
-            requestBody: {
-                majorDimension: 'ROWS',
-                values: $values
-            }
-        })
+            return sheet.values.update({
+                spreadsheetId: process.env.SHEETS_URL,
+                range: $range,
+                valueInputOption: 'USER_ENTERED',
+                includeValuesInResponse: true,
+                requestBody: {
+                    majorDimension: 'ROWS',
+                    values: $values
+                }
+            })
+        } catch (e: any) {
+            throw new GoogleSheetsError(500, e.message);
+        }
+        
     }
 
     public static async append($values: any[][], $range: string) {
-        const sheet = await this.getDbContext();
+        try {
+            const sheet = await this.getDbContext();
 
-        return sheet.values.append({
-            spreadsheetId: process.env.SHEETS_URL,
-            range: $range,
-            valueInputOption: 'USER_ENTERED',
-            includeValuesInResponse: true,
-            requestBody: {
-                majorDimension: 'ROWS',
-                values: $values
-            }
-        })
+            return sheet.values.append({
+                spreadsheetId: process.env.SHEETS_URL,
+                range: $range,
+                valueInputOption: 'USER_ENTERED',
+                includeValuesInResponse: true,
+                requestBody: {
+                    majorDimension: 'ROWS',
+                    values: $values
+                }
+            })  
+        } catch (e: any) {
+            throw new GoogleSheetsError(500, e.message);
+        }
     }
 
     public static async getByMatch(
@@ -73,41 +95,50 @@ class SheetsDbContext {
         $colStart: string, 
         $colEnd: string
     ) {
-        const values = await SheetsDbContext.get(`${$page}!${$colSearch}:${$colSearch}`);
+        try {
+            const values = await SheetsDbContext.get(`${$page}!${$colSearch}:${$colSearch}`);
 
-        if (!values || !values.data.values)
-            return undefined;
+            if (!values || !values.data.values)
+                return undefined;
 
-        const rowSearch = values.data.values?.findIndex(r => r[0] == $value);
+            const rowSearch = values.data.values?.findIndex(r => r[0] == $value);
 
-        return rowSearch == -1? 
-            undefined : 
-            await SheetsDbContext.get(`${$page}!${$colStart}${rowSearch + 1}:${$colEnd}${rowSearch + 1}`);
+            return rowSearch == -1? 
+                undefined : 
+                await SheetsDbContext.get(`${$page}!${$colStart}${rowSearch + 1}:${$colEnd}${rowSearch + 1}`);
+        } catch (e: any) {
+            throw new GoogleSheetsError(500, e.message);
+        }
     }
 
     public static async deleteRow(
         $sheetId: number,
         $rowNumber: number
     ) {
-        const sheet = await this.getDbContext();
+        try {
+            const sheet = await this.getDbContext();
 
-        const response = sheet.batchUpdate({
-            spreadsheetId: process.env.SHEETS_URL,
-            requestBody: {
-                requests: [
-                    {
-                        deleteDimension: {
-                            range: {
-                                sheetId: $sheetId,
-                                dimension: 'ROWS',
-                                startIndex: $rowNumber,
-                                endIndex: $rowNumber + 1
+            const response = sheet.batchUpdate({
+                spreadsheetId: process.env.SHEETS_URL,
+                requestBody: {
+                    requests: [
+                        {
+                            deleteDimension: {
+                                range: {
+                                    sheetId: $sheetId,
+                                    dimension: 'ROWS',
+                                    startIndex: $rowNumber,
+                                    endIndex: $rowNumber + 1
+                                }
                             }
                         }
-                    }
-                ]
-            }
-        })
+                    ]
+                }
+            })
+        } catch (e: any) {
+            throw new GoogleSheetsError(500, e.message);
+        }
+        
     }
 }
 

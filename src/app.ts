b882ff9ba960da1acfa2de7973b5440f9b424bb2
Lucas Gomes Dias <lucasgomesdias1404@gmail.com>
2026-04-30 11:26:04 -0300

    feat: Adicionado rota de controllers em App.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from "express";
 import * as dotenv from 'dotenv'
+import deviceRouter from "./routers/device/Device.routers.ts";
+import loginRouter from "./routers/login/Login.router.ts";
 
 dotenv.config();
 
 const APP = express();
 const PORT = 3000;
 
-APP.get('/', (req: Request, res: Response) => {
-    res.send("Funcionando!");
-});
+
+APP.use(express.json())
+APP.use("/device", deviceRouter);
+APP.use("/login", loginRouter);
 
 APP.listen(PORT, () => {
     console.log(`Sistema rondando em https://localhost:${PORT}`);

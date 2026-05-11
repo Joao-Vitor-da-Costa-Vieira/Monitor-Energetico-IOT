0797f101389bfb47ff0802fbb64ca2ff0a1bde16
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-05-11 12:54:44 -0300

    fix: alteração do SafeView para Scroll

--- src/frontEnd/MonitorIOT/components/safeView.jsx
+++ src/frontEnd/MonitorIOT/components/safeView.jsx
@@ -1,17 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+// components/safeView.js
+import { StyleSheet, ScrollView, View } from 'react-native'
 import React from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-const SafeView = ({style, safe = false, ...props}) => {
+const SafeView = ({ style, safe = false, scrollable = true, children, ...props }) => {
   const insets = useSafeAreaInsets()
 
+  const containerStyle = [
+    {
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingRight: insets.right,
+      flex: 1,
+    },
+    style,
+  ]
+
+  if (scrollable) {
+    return (
+      <ScrollView 
+        style={containerStyle}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollContent, { paddingBottom: 100 }}
+        {...props}
+      >
+        {children}
+      </ScrollView>
+    )
+  }
+
   return (
-    <View style={[{paddingTop: insets.top, paddingBottom: insets.bottom, paddingLeft: insets.left, paddingRight: insets.right,},style]}
-    {...props}
-    />
+    <View style={containerStyle} {...props}>
+      {children}
+    </View>
   )
 }
 
 export default SafeView
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  scrollContent: {
+    flexGrow: 1,
+  },
+})

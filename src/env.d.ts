cfda7e978de2679bf41bacea24807fd4c11ceab7
Lucas Gomes Dias <theminimare4514@gmail.com>
2026-05-16 17:12:45 -0300

    fix: Pull da branch main

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             SHEETS_URL: string;
+            ENCRYPT_KEY: string;
             GOOGLE_CREDENTIALS: string;
         }
     }

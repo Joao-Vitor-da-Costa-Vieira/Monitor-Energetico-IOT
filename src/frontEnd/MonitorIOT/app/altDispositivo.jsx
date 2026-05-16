6840b0eeee839c39fe4adc3a74b22e47965142b6
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-05-16 18:42:19 -0300

    fix: Correção nos Inputs de Texto de altDispositivo

--- src/frontEnd/MonitorIOT/app/altDispositivo.jsx
+++ src/frontEnd/MonitorIOT/app/altDispositivo.jsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
-import React from 'react'
+import React, {useState} from 'react'
 
 //componentes
 import  SafeView  from '../components/safeView'
@@ -10,13 +10,21 @@ import buttons from '../components/buttons'
 const altDispositivo = () => {
   const { id, title, place } = useLocalSearchParams()
 
+ const [newTitle, setNewTitle] = useState(title || '')
+  const [newPlace, setNewPlace] = useState(place || '')
+
+  const handleAlterar = () => {
+
+    console.log('Dispositivo alterado:', { id, newTitle, newPlace })
+  }
+
   return (
     <SafeView>
       <View style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 10}}>
         <Text style={{ fontSize: 20, fontWeight: 'bold', paddingBottom: 5 }}>Alterar o Dispositivo</Text>
-        <TextoInputs placeholder={title} />
-        <TextoInputs placeholder={place || 'Localização do Dispositivo'} />
-        {buttons({buttonProps: {onPress: () => console.log('Pressed'), title: 'Alterar'}})}
+        <TextoInputs placeholder={title} value={newTitle} onChangeText={setNewTitle} />
+        <TextoInputs placeholder={place || 'Localização do Dispositivo'} value={newPlace} onChangeText={setNewPlace} />
+        {buttons({buttonProps: {onPress: handleAlterar, title: 'Alterar'}})}
       </View>
     </SafeView>
   )

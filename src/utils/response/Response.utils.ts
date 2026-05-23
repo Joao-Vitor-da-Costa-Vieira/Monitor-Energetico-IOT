e7c359b83eed390b9531d5ca9783c14792272420
Lucas Gomes Dias <theminimare4514@gmail.com>
2026-05-23 20:47:30 -0300

    feat: ResponseUtils

---
+++ src/utils/response/Response.utils.ts
@@ -0,0 +1,30 @@
+import { AbstractApiError } from "../../errors/AbstractApi.error.ts";
+import { Response } from "express";
+
+export abstract class ResponseUtils {
+    /**
+     * ReturnErrorResponse
+     * @param {AbstractApiError} e
+     */
+    public static ReturnApiErrorResponse(res: Response, e: AbstractApiError) {
+        console.error(e.$consoleLog);
+        return res.status(e.$statusCode).send({"message": e.$message});
+    }
+
+    /**
+     * returnGenericErrorResponse
+     */
+    public static returnGenericErrorResponse(res: Response, e: Error, httpCode: number = 500) {
+        console.error(e);
+        return res.status(httpCode).send({"message": e.message});
+    }
+
+    /**
+     * FormatSucessResponse
+     * @param {number} httpCode
+     * @param {Object} obj
+     */
+    public static ReturnObjectResponse(res: Response, obj: Object, httpCode: number = 200) {
+        return res.status(200).send(obj);
+    }
+}

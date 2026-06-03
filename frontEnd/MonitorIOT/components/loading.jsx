a7fead0e04e4bb6d97256698289983a8b646a963
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-06-03 11:18:59 -0300

    feat: loading component

---
+++ frontEnd/MonitorIOT/components/loading.jsx
@@ -0,0 +1,23 @@
+import { View, ActivityIndicator } from 'react-native'
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" style={styles.activityIndicator} />
+    </View>
+  )
+}
+
+export default Loading
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  activityIndicator: {
+    color: '#01cfeb',
+  },
+})

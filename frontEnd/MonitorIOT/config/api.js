a3c31a6c5483643b7321e5f2c53aa60e75bc8b28
Joao-Vitor-da-Costa-Vieira <jvcvieira2001@hotmail.com>
2026-06-03 12:03:13 -0300

    feat: Config das APIs de login

--- frontEnd/MonitorIOT/config/api.js
+++ frontEnd/MonitorIOT/config/api.js
@@ -6,10 +6,12 @@ export const API_CONFIG = {
   BASE_URL: getApiUrl(),
   ENDPOINTS: {
     CREATE_ACCOUNT: '/user/CreateAccount',
-    GET_USER: '/user/GetUser',
-    UPDATE_ACCOUNT: '/user/UpdateAccount',
-    DEACTIVATE_ACCOUNT: '/user/DeactivateAccount',
-    ACTIVATE_ACCOUNT: '/user/ActivateAccount'
+    GET_USER: '/user/GetUser/',
+    UPDATE_ACCOUNT: '/user/UpdateAccount/',
+    DEACTIVATE_ACCOUNT: '/user/DeactivateAccount/',
+    ACTIVATE_ACCOUNT: '/user/ActivateAccount/',
+    GET_LOGGED_USER: '/login/GetLoggedUser/',
+    UPDATE_ACCOUNT: '/login/UpdateAccount/'
   }
 };
 
